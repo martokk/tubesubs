1206c0c2178b7bc94fad0132e651b7dd0019278b
martokk <25113632+martokk@users.noreply.github.com>
2023-02-01 20:28:44 -0600

    add register post, bugfix template

--- python_fastapi_stack/views/endpoints/login.py
+++ python_fastapi_stack/views/endpoints/login.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException, Request, status
+import httpx
+from fastapi import APIRouter, Depends, Form, HTTPException, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from fastapi.templating import Jinja2Templates
+from pydantic import EmailStr
 from sqlmodel import Session
 
-from python_fastapi_stack import settings
+from python_fastapi_stack import logger, models, settings
 from python_fastapi_stack.api.deps import get_db
 from python_fastapi_stack.core import security
 
@@ -13,18 +15,22 @@ templates = Jinja2Templates(directory="python_fastapi_stack/views/templates")
 
 
 @router.get("/login", response_class=HTMLResponse)
-async def login(request: Request) -> Response:
+async def login(request: Request, success: bool | None = None) -> Response:
     """
     Login Page.
 
     Args:
         request(Request): The request object
+        success(bool): Whether the user was created successfully
 
     Returns:
         Response: Login page
 
     """
-    return templates.TemplateResponse("login/login.html", {"request": request})
+    alerts = models.Alerts()
+    if success:
+        alerts.success.append("User created successfully")
+    return templates.TemplateResponse("login/login.html", {"request": request, "alerts": alerts})
 
 
 @router.post("/login", response_class=HTMLResponse)
@@ -90,4 +96,87 @@ async def register(
     return templates.TemplateResponse(
         "login/register.html",
         {"request": request, "registration_enabled": settings.USERS_OPEN_REGISTRATION},
+    )
+
+
+@router.post("/register", response_class=HTMLResponse)
+async def handle_registration(
+    request: Request,
+    username: str = Form(None),
+    password: str = Form(None),
+    password_confirmation: str = Form(None),
+    email: str = Form(None),
+    full_name: str = Form(None),
+) -> Response:
+    """
+    Handle registration.
+    # TODO: Add captcha
+
+    Args:
+        request(Request): The request object
+        username(str): The username
+        password(str): The password
+        password_confirmation(str): The password confirmation
+        email(str): The email
+        full_name(str): The full name
+
+    Returns:
+        Response: Registration page
+
+    Raises:
+        HTTPException: Registration is disabled
+    """
+    alerts = models.Alerts()
+    if not settings.USERS_OPEN_REGISTRATION:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Registration is disabled"
+        )
+
+    # Check if all fields are filled
+    if not all([username, password, password_confirmation, email, full_name]):
+        alerts.danger.append("Please fill out all fields")
+
+    # Confirm passwords match
+    if password != password_confirmation:
+        alerts.danger.append("Passwords do not match")
+
+    # Validate email via pydantic
+    valid_email = None
+    try:
+        valid_email = EmailStr.validate(email)
+    except ValueError:
+        alerts.danger.append("Invalid email")
+
+    # Post to the API if there are no errors
+    if not alerts.danger:
+        endpoint = f"{settings.BASE_URL}{settings.API_V1_PREFIX}/register"
+        data = {
+            "username": username,
+            "password": password,
+            "email": valid_email,
+            "full_name": full_name,
+        }
+        async with httpx.AsyncClient() as client:
+            response = await client.post(url=endpoint, json=data, timeout=2)
+
+        # Handle response
+        if response.status_code == status.HTTP_201_CREATED:
+            return RedirectResponse("/login?success=true", status_code=status.HTTP_302_FOUND)
+        elif response.status_code == status.HTTP_409_CONFLICT:
+            alerts.danger.append("Username or email already exists")
+        elif response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY:
+            alerts.danger.append("Invalid data")
+            logger.error(f"Error registering user: {response.status_code=}, {response.text=}")
+        else:
+            logger.error(f"Error registering user: {response.status_code=}, {response.text=}")
+            alerts.danger.append("Error registering user")
+
+    # Return the registration page with alert messages
+    return templates.TemplateResponse(
+        "login/register.html",
+        {
+            "request": request,
+            "registration_enabled": settings.USERS_OPEN_REGISTRATION,
+            "alerts": alerts,
+        },
     )
