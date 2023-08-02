1f54a94960cc2cf5b5a2ec5a8df34723b66c0919
martokk <25113632+martokk@users.noreply.github.com>
2023-08-02 16:01:50 -0500

    cleanup

--- app/models/filtered_videos.py
+++ app/models/filtered_videos.py
@@ -1,19 +1,12 @@
-from typing import TYPE_CHECKING
-
 from sqlmodel import SQLModel
-
-if TYPE_CHECKING:
-    from app.models.criteria import Criteria
-    from app.models.video import Video
 
 
 class FilteredVideos(SQLModel):
-    videos: list
+    videos: list  # type: ignore
     videos_limited_count: int
     videos_not_limited_count: int
     read_status: str
     show_hidden_channels: bool
-    criterias: list
-    ordered_by: str
+    criterias: list  # type: ignore
     reverse_order: bool
     limit: int
